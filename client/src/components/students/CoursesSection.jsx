ae4f28ab35c59e59b36f59f7ddd73c87ca249b67
Prashant Kumar <prashantbaghel98@gmail.com>
2026-06-02 16:50:37 +0530

    env

--- client/src/components/students/CoursesSection.jsx
+++ client/src/components/students/CoursesSection.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const CoursesSection = () => {
   return (
     <div>
-      
+      <h2>Learn from the best</h2>
+      <p>Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
     </div>
   )
 }
